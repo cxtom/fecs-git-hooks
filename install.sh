f086599182304e167d7a68d8e090e7eb2abdaeee
ChenXiao <chenxiao07@baidu.com>
2015-05-28 19:39:04 +0800

    尝试npm打包

--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 git clone https://github.com/cxtom/fecs-git-hooks.git
-cd fecs-git-hooks
-
 echo "拷贝文件...."
 
-git config --global init.templatedir '~/.git_template'
-\cp -f hooks/pre-commit ~/.git_template/hooks/
+# git config --global init.templatedir '~/.git_template'
+# \cp -f hooks/pre-commit ~/.git_template/hooks/
 
-echo "#####################################################"
-echo "#                  installed!                       #"
-echo "#   Please process 'git init' in your project       #"
-echo "#   Or copy './hooks/pre-commit' to '.git/hooks'!   #"
-echo "#####################################################"
+root=$(cd `dirname $0`; pwd)
+gitPath=$root"/.git/"
+
+if [ ! -d "$gitPath" ]; then
+    echo "非git仓库，退出"
+else
+    cd fecs-git-hooks
+    cp -f hooks/pre-commit ../.git/hooks/
+    cd ..
+
+    echo "###################################################"
+    echo "#                  初始化完成                       #"
+    echo "###################################################"
+fi
+
+rm -rf fecs-git-hooks
