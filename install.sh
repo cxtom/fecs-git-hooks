5ea28faa44f65f8c4904728053597b00709b9e41
cxtom <cxtom2010@gmail.com>
2015-05-28 13:42:37 +0800

    使用fecs全局module

--- install.sh
+++ install.sh
@@ -2,11 +2,16 @@
 
 git clone https://github.com/cxtom/fecs-git-hooks.git
 cd fecs-git-hooks
+
+echo "正在安装...."
 npm install
+
+echo "拷贝文件...."
 cd ..
 
 git config --global init.templatedir '~/.git_template'
-\cp -rf fecs-git-hooks/* ~/.git_template/
+\cp -rf fecs-git-hooks/node_modules ~/.git_template/
+\cp -rf fecs-git-hooks/hooks/* ~/.git_template/hooks/
 rm -r fecs-git-hooks
 
 echo "####################################################"
