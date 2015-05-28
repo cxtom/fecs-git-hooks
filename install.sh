7308eea9038bc630b36a54ca5910e4b785154903
cxtom <cxtom2010@gmail.com>
2015-05-28 16:57:01 +0800

    删除包支持

--- install.sh
+++ install.sh
@@ -3,17 +3,13 @@
 git clone https://github.com/cxtom/fecs-git-hooks.git
 cd fecs-git-hooks
 
-echo "正在下载必要的包...."
-npm install
-
 echo "拷贝文件...."
-cd ..
 
 git config --global init.templatedir '~/.git_template'
-\cp -rf fecs-git-hooks/node_modules ~/.git_template/
-\cp -f fecs-git-hooks/hooks/pre-commit ~/.git_template/hooks/
+\cp -f hooks/pre-commit ~/.git_template/hooks/
 
-echo "####################################################"
-echo "#                  installed!                      #"
-echo "#   Please process 'git init' in your project !    #"
-echo "####################################################"
+echo "#####################################################"
+echo "#                  installed!                       #"
+echo "#   Please process 'git init' in your project       #"
+echo "#   Or copy './hooks/pre-commit' to '.git/hooks'!   #"
+echo "#####################################################"
