083c634ddcbcefe32c1ab1545c86ab9dd0defd73
cxtom <cxtom2010@gmail.com>
2015-05-28 15:23:56 +0800

    添加对新建仓库的支持

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/cxtom/fecs-git-hooks.git
 cd fecs-git-hooks
 
-echo "正在安装...."
+echo "正在下载必要的包...."
 npm install
 
 echo "拷贝文件...."
@@ -11,8 +11,7 @@ cd ..
 
 git config --global init.templatedir '~/.git_template'
 \cp -rf fecs-git-hooks/node_modules ~/.git_template/
-\cp -rf fecs-git-hooks/hooks/* ~/.git_template/hooks/
-rm -r fecs-git-hooks
+\cp -f fecs-git-hooks/hooks/pre-commit ~/.git_template/hooks/
 
 echo "####################################################"
 echo "#                  installed!                      #"
